22a58324d0086ec8eaa5630372092de76a31c9f9
Ronnakrit888 <ronnakrit.t@kkumail.com>
2025-09-23 18:18:30 +0700

    optimize code

--- include/gpio_functions.c
+++ include/gpio_functions.c
@@ -1,7 +1,7 @@
 #include "gpio_types.h"
 #include "stm32f4xx.h"
 
-void GPIO_Button_Init(GPIO_TypeDef* GPIOx, uint8_t pin_number, GPIO_Pull_Type pull_config) {
+void GPIO_Button_Init(GPIO_TypeDef* GPIOx, uint8_t pin_number, MY_GPIO_Pull_Type pull_config) {
 
     // Clear MODER bits
     GPIOx->MODER &= ~(0b11 << (pin_number * 2));
--- include/gpio_types.h
+++ include/gpio_types.h
@@ -5,41 +5,41 @@
 
 // GPIO Pull Configuration enumuration
 typedef enum {
-    GPIO_PULL_NONE = 0b00,
-    GPIO_PULL_UP = 0b01,
-    GPIO_PULL_DOWN = 0b10,
-    GPIO_PULL_RESERVED = 0b11
-} GPIO_Pull_Type;
+    MY_GPIO_PULL_NONE = 0b00,
+    MY_GPIO_PULL_UP = 0b01,
+    MY_GPIO_PULL_DOWN = 0b10,
+    MY_GPIO_PULL_RESERVED = 0b11
+} MY_GPIO_Pull_Type;
 
 // GPIO Mode enumuration
 typedef enum {
-    GPIO_MODE_INPUT = 0b00,
-    GPIO_MODE_OUTPUT = 0b01,
-    GPIO_MODE_ALTERNATIVE = 0b10,
-    GPIO_MODE_ANALOG = 0b11
-} GPIO_Mode_Type;
+    MY_GPIO_MODE_INPUT = 0b00,
+    MY_GPIO_MODE_OUTPUT = 0b01,
+    MY_GPIO_MODE_ALTERNATIVE = 0b10,
+    MY_GPIO_MODE_ANALOG = 0b11
+} MY_GPIO_Mode_Type;
 
 // GPIO Output Type enumuration
 typedef enum {
-    GPIO_OTYPE_PUSH_PULL  = 0,
-    GPIO_OTYPE_OPEN_DRAIN = 1
-} GPIO_OutputType_Type;
+    MY_GPIO_OTYPE_PUSH_PULL  = 0,
+    MY_GPIO_OTYPE_OPEN_DRAIN = 1
+} MY_GPIO_OutputType_Type;
 
 // GPIO Speed enumeration
 typedef enum {
-    GPIO_SPEED_LOW    = 0b00,
-    GPIO_SPEED_MEDIUM = 0b01,
-    GPIO_SPEED_HIGH   = 0b10,
-    GPIO_SPEED_VERY_HIGH = 0b11
-} GPIO_Speed_Type;
+    MY_GPIO_SPEED_LOW    = 0b00,
+    MY_GPIO_SPEED_MEDIUM = 0b01,
+    MY_GPIO_SPEED_HIGH   = 0b10,
+    MY_GPIO_SPEED_VERY_HIGH = 0b11
+} MY_GPIO_Speed_Type;
 
 // Button state enumeration
 typedef enum {
-    BUTTON_RELEASED = 0,
-    BUTTON_PRESSED  = 1,
-    BUTTON_DEBOUNCE = 2
-} Button_State_Type;
+    MY_BUTTON_RELEASED = 0,
+    MY_BUTTON_PRESSED  = 1,
+    MY_BUTTON_DEBOUNCE = 2
+} MY_Button_State_Type;
 
-void GPIO_Button_Init(GPIO_TypeDef* GPIOx, uint8_t pin_number, GPIO_Pull_Type pull_config);
+void GPIO_Button_Init(GPIO_TypeDef* GPIOx, uint8_t pin_number, MY_GPIO_Pull_Type pull_config);
 
 #endif
